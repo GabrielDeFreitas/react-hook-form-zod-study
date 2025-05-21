843bbecab1b1eb702680a62f1783d87672498498
GabrielDeFreitas <gabrielfreitas.ilustra@gmail.com>
2025-05-20 22:29:06 -0300

    fix: a11y countries aria-invalid

--- src/App.tsx
+++ src/App.tsx
@@ -37,7 +37,7 @@ export default function App() {
 
       <div className="mb-5">
         <label htmlFor="countries" className="block mb-2 text-sm font-medium text-gray-900">Select your country</label>
-        <select {...register('countries')} id="countries" aria-invalid={!!errors.name} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+        <select {...register('countries')} id="countries" aria-invalid={!!errors.countries} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
           <option value="">Choose a country</option>
           <option value="US">United States</option>
           <option value="CA">Canada</option>
