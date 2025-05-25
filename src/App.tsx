6cf1a6da5fc18fefd7864de7152719bc82c6781b
GabrielDeFreitas <gabrielfreitas.ilustra@gmail.com>
2025-05-25 00:15:53 -0300

    fix: type date

--- src/App.tsx
+++ src/App.tsx
@@ -38,7 +38,7 @@ export default function App() {
 
       <div className="mb-5">
         <label htmlFor="dateOfBirth" className="block mb-2 text-sm font-medium text-gray-900">Date of birth</label>
-        <input type="dateOfBirth" {...register('dateOfBirth')} id="dateOfBirth" aria-invalid={!!errors.dateOfBirth} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" />
+        <input type="date" {...register('dateOfBirth')} id="dateOfBirth" aria-invalid={!!errors.dateOfBirth} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" />
         {errors.dateOfBirth && <span role="alert" className="mt-2 text-sm text-red-600">{errors.dateOfBirth.message}</span>}
       </div>
 
